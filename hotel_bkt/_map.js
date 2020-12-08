830c9365dffcad98589aaae7f65ceb45b0d76900
anggi-julieta <63390772+anggi-julieta@users.noreply.github.com>
2020-12-08 17:54:14 +0700

    Update _map.js

--- hotel_bkt/_map.js
+++ hotel_bkt/_map.js
@@ -1,5 +1,5 @@
         var map;
-        var server = ipServerHotel;
+        var server = https://anggijulieta28webgishoteliisi.herokuapp.com/;
         var cekRadiusStatus = "off";   //RADIUS
         var circles = []; //RADIUS
         var rad; //RADIUS
